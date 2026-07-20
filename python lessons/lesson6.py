ebd3dc27d02e4b14a8f8d4f70399168199198d7d
Thabang-Magaele <magaelethabang@gmail.com>
2026-07-20 23:58:05 +0200

    Working with Lists and dictionaries

---
+++ python lessons/lesson6.py
@@ -0,0 +1,21 @@
+# working with lists
+cart = ["Apples", "Eggs", "Milk"]
+print(cart[0])
+
+cart.append("Bread")
+print(cart)
+
+for item in cart:
+    print(item)
+
+# working with dictionaries
+student = {
+    "name" : "Thabang",
+    "age" : "20",
+    "course": "Introductiion to Python"
+
+}
+
+print (student["name"])
+print (student["age"])
+print (student["course"])
