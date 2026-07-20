4ebaf7f1890a5c31785479a08e7d6aa528935ae4
Thabang-Magaele <magaelethabang@gmail.com>
2026-07-20 11:57:56 +0200

    Grade Classifier Script

--- assignments/grade_classifier.py
+++ assignments/grade_classifier.py
@@ -6,49 +6,45 @@
 #Display a formatted report card showing all inputs, the average, the grade, the status, and any intervention flags
 
 full_name = input("Enter your name and surname: ").strip()
-subject1 = float(input("Enter your mark for Subject 1: "))
-subject2 = float(input("Enter your mark for Subject 2: "))
-subject3 = float(input("Enter your mark for Subject 3: "))
 
-#Claculate average mark
-average_mark = (subject1 + subject2 + subject3) / 3
+mark1 = float(input("Enter your mar for subject 1: "))
+mark2 = float(input("Enter your mar for subject 2: "))
+mark3 = float(input("Enter your mar for subject 3: "))
 
-#Assigning a letter grade
-if average_mark >= 80:
+average_mark = (mark1 + mark2 + mark3) /3
+
+if average_mark >=80:
     letter_grade = "A"
     status = "Pass"
-
 elif average_mark >=70 and average_mark <=79:
     letter_grade = "B"
     status = "Pass"
-
 elif average_mark >=60 and average_mark <=69:
     letter_grade = "C"
     status = "Pass"
-
 elif average_mark >=50 and average_mark <=59:
     letter_grade = "D"
     status = "Pass"
-
 else:
     letter_grade = "F"
     status = "Fail"
 
-if subject1 <40:
-    attention = "subject 1 NEEDS ATTENTION"
-elif subject2 <40:
-    attention = "subject 2 NEEDS ATTENTION"
-elif subject3 <40:
-    attention = "subject 3 NEEDS ATTENTION"
+if mark1 <40:
+    flag = "Subject 1 Needs Attention!"
+elif mark2<40:
+    flag = "Subject 2 Needs Attention!"
+elif mark3<40:
+    flag = "Subject 3 Needs Attention!"
 else:
-    attention = "No attention needed"
+    flag = "No attention needed"
 
-print ("\nReport Card")
-print ("======================================")
-print (f"Full Name: {full_name}")
-print (f"Subject 1 mark: {round(subject1, 2)}")
-print (f"Subject 2 mark: {round(subject2, 2)}")
-print (f"Subject 3 mark: {round(subject3, 2)}")
-print ("======================================")
+print(f"\nReport Card for: {full_name}")    
+print("===================================")
+print(f"Subject 1: {round(mark1, 2)}")
+print(f"Subject 2: {round(mark2, 2)}")
+print(f"Subject 3: {round(mark3, 2)}")
+print("===================================")
 print (f"Average Mark: {round(average_mark, 2)}")
-print (f"Grade Symbol: {letter_grade}\nStatus: {status}\nIntervention Flags: {attention}")
+print(f"Grade Symbol: {letter_grade}")
+print(f"Pass/Fail: {status}")
+print(f"Intervention: {flag}")
